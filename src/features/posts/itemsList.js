6726009c1d0829f545b4c58aa28a33e0aac34ab0
thayorn <thayornswordsman@gmail.com>
2024-02-16 18:45:17 +0300

    RTK#auto-fetch

--- src/features/posts/itemsList.js
+++ src/features/posts/itemsList.js
@@ -4,51 +4,58 @@ import { useGetPostsQuery } from "../api/apiSlice"
 import { useEffect, useState } from "react"
 
 const ItemsList = () => {
-    const [limit, setLimit] = useState(5)
+    const [limit, setLimit] = useState(10)
     const { data: posts, isLoading, isSuccess, isError, error, refetch } = useGetPostsQuery({ limit, start: 0 })
-
-    const handleScroll = () => {
-        if (window.innerHeight + document.documentElement.scrollTop !== document.documentElement.offsetHeight) return
-        setLimit(limit + 5)
-    }
-    
-    useEffect(() => {
-        refetch()
-          .unwrap()
-      }, [refetch])
-
-    useEffect(() => {
-        window.addEventListener('scroll', handleScroll)
-        return () => window.removeEventListener('scroll', handleScroll)
-    }, )
-
+    const [newPosts, setNewPosts] = useState([])
+    const [isFetching, setIsFetching] = useState(false)
     let content
-
-    if(isLoading){
-        content = <Spin />
-    }else if(isSuccess){
-        content = posts.map((item,index) => (
-            <li className='li-container' key={index}>
-                <span># {item.id}</span>
-                <h2>{item.title.slice(0, 6)}</h2>
-                <p>{item.body}</p>
-                <Link to={`/posts/${item.id}`} className="button-view-post">
-                    View Post
-                </Link>
-            </li>)    
-        )
-    }else if(isError){
-        content = <div id="error-page">Error: 'something went wrong' {error.message}</div>
-    }
     
-
+    const handleScroll = () => {
+      if (window.innerHeight + document.documentElement.scrollTop !== document.documentElement.offsetHeight || isFetching) return
+      setLimit((prevLimit) => prevLimit + 5)
+      // setNewPosts((prevNewPosts) => [...prevNewPosts, ...posts])
+      setIsFetching(true)
+    }
+  
+    useEffect(() => {
+      if (!isFetching) return
+      refetch().unwrap().then(() => {
+        setIsFetching(false)
+      })
+    }, [isFetching, posts, refetch])
+  
+    useEffect(() => {
+      window.addEventListener('scroll', handleScroll)
+      return () => window.removeEventListener('scroll', handleScroll)
+    }, )
+    
+  
+    if (isLoading) {
+      content = <Spin />
+    } else if (isSuccess) {
+      content = posts.map((item, index) => (
+        <li className='li-container' key={index}>
+          <span># {item.id}</span>
+          <h2>{item.title.slice(0, 6)}</h2>
+          <p>{item.body}</p>
+          <Link to={`/posts/${item.id}`} className="button-view-post">
+            View Post
+          </Link>
+        </li>
+      ))
+    } else if (isError) {
+      content = <div id="error-page">Error: Something went wrong {error.message}</div>
+    }
+  
     return (
-        <section>
-            <h1>Posts</h1>
-            <>
-                {content}
-            </>
-        </section>
+      <section>
+        <h1>Posts</h1>
+        <ul>
+          {content}
+          {isFetching && (<ul className="newposts-loading">loading...</ul>)}
+        </ul>
+      </section>
     )
-}
-export default ItemsList
+  }
+  
+  export default ItemsList
