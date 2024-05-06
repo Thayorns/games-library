0224f08d53f75427d42ece098279a69c3c150abb
valentine <thayornswordsman@gmail.com>
2024-05-06 09:53:31 +0300

    библиотека игр. готов весь функционал фильтрации

--- src/features/api/apiSlice.ts
+++ src/features/api/apiSlice.ts
@@ -6,14 +6,14 @@ export const apiSlice = createApi({
   endpoints: (builder) => ({
     getGames: builder.query({
     // количество игр на странице
-      query: ({ page = 1, pageSize = 10 }) => `
+      query: ({ page = 1, pageSize = 20 }) => `
         https://api.rawg.io/api/games?key=71c319a9886e4b2283f2eacd42d57b7d&page=${page}&page_size=${pageSize}
         `,
     }),
     // детали игры по id
     getGame: builder.query({
       query: (itemId) => `
-        https://api.rawg.io/api/games?key=71c319a9886e4b2283f2eacd42d57b7d&${itemId}
+        https://api.rawg.io/api/games/${itemId}?key=71c319a9886e4b2283f2eacd42d57b7d&
       `
     })
   }),
--- src/features/posts/itemsList.tsx
+++ src/features/posts/itemsList.tsx
@@ -6,8 +6,8 @@ import { useEffect, useState } from "react"
 import { Button, FloatButton, Popover, Carousel } from 'antd'
 
 const ItemsList = () => {
-  const [pageSize, setPageSize] = useState(10)
-  const { data: games, isLoading, isSuccess, isError, error, refetch } = useGetGamesQuery({ pageSize, page: 1 })
+  const [pageSize, setPageSize] = useState(50)
+  const { data: games, isLoading, isSuccess, isError, error, refetch } = useGetGamesQuery({ pageSize, start: 0 })
   // const [newGames, setNewGames] = useState<any[]>([])
   const [isFetching, setIsFetching] = useState(false)
   // инициализируем состояния для селектов
@@ -17,6 +17,7 @@ const ItemsList = () => {
   const [language, setLanguage] = useState('')
   let content
   console.log(games);
+  
   
   // const handleClick = (e: any) => {
   //   if(e.type === 'click'){ //e.type
@@ -37,8 +38,10 @@ const ItemsList = () => {
   }, [isFetching, games, refetch])
   
   // инициализируем типы для отрисовки игр в соответствии с rawg.io
+  
   type Tag = {
     name: 'Singleplayer' | 'Multiplayer' | 'Co-op'
+    language: 'eng' | 'ru'
   }
   type ImagesScreenshot = {
     image: string
@@ -63,47 +66,60 @@ const ItemsList = () => {
     rating: number
     background_image: string
   }
+  const filterGamesByCriteria = (games:any, platform:string, gameType:string, rating:string, language:string) => {
+    return games.filter((game:any) => {
+      return (
+        (!platform || game.platforms.some((platformItem: any) => platformItem.platform.name.toLowerCase() === platform.toLowerCase())) &&
+        (!gameType || game.tags.some((tag:any) => tag.name.toLowerCase() === gameType.toLowerCase())) &&
+        (!rating || (rating === '>4 rate' ? game.rating > 4 : game.rating < 4)) &&
+        (!language || game.tags.some((tag:any) => tag.language.toLowerCase() === language.toLowerCase()))
+      );
+    });
+  };
   // кондиции для отрисовки
-  if (isLoading) {
-    content = <Spin />
-  } else if (isSuccess) {
+if (isLoading) {
+  content = <Spin />
+} else if (isSuccess) {
+  const filteredGames = filterGamesByCriteria(games.results, platform, gameType, rating, language)
     
-    content = games.results.map((item: mappedGame, index: number) => {
-    
-    const screenshotContent = (
-      <Carousel autoplay>
-        {item.short_screenshots.map((screenshotItem, screenshotIndex)=> (
-          <div className="screenshots" style={contentStyle} key={screenshotIndex}>
-            <img className="screenshots-img" src={screenshotItem.image} alt=""/>
-          </div>
-        ))}
-      </Carousel>
-    )
-    
-    return (
-      <li className='li-container' key={index}>
-        <Popover content={screenshotContent} title="Скриншоты" trigger="click">
-          <img src={item.background_image} alt=""/>
-        </Popover>
-        <div className="div-content-container">
-          {/* <span># {item.id}</span> */}
-          <h2>{item.name}</h2>
-          <span><strong>rating {item.rating} </strong></span>
-          <Link to={`/games/${item.id}`} className="button-view-post">
-            <Button>описание</Button>
-          </Link>
+  content = filteredGames.map((item: mappedGame, index: number) => {
+  
+  const screenshotContent = (
+    <Carousel autoplay>
+      {item.short_screenshots.map((screenshotItem, screenshotIndex)=> (
+        <div className="screenshots" style={contentStyle} key={screenshotIndex}>
+          <img className="screenshots-img" src={screenshotItem.image} alt=""/>
         </div>
-        <div className="div-platforms">
-          {item.platforms.map((platformItem, platformIndex) => (
-            <span className="platform-item" key={platformIndex}>{platformItem.platform.name} |</span>
-          ))} <br/>
-          {item.tags.filter(tag => ['Singleplayer', 'Multiplayer', 'Co-op'].includes(tag.name)).map((tagsItem, tagsIndex) => 
-            (
-              <span className="platform-item" key={tagsIndex}>{tagsItem.name} |</span>
-            ))}
-        </div>
-      </li>
-    )})
+      ))}
+    </Carousel>
+  )
+  
+  return (
+    <li className='li-container' key={index}>
+      <Popover content={screenshotContent} title="Скриншоты" trigger="click">
+        <img src={item.background_image} alt=""/>
+      </Popover>
+      <div className="div-content-container">
+        <h2>{item.name}</h2>
+        <span>
+          <strong>
+            rating {item.rating}
+          </strong></span>
+        <Link to={`/games/${item.id}`} className="button-view-post">
+          <Button>описание</Button>
+        </Link>
+      </div>
+      <div className="div-platforms">
+        {item.platforms.map((platformItem, platformIndex) => (
+          <span className="platform-item" key={platformIndex}>{platformItem.platform.name} |</span>
+        ))} <br/>
+        {item.tags.filter(tag => ['Singleplayer', 'Multiplayer', 'Co-op'].includes(tag.name)).map((tagsItem, tagsIndex) => 
+          (
+            <span className="platform-item" key={tagsIndex}>{tagsItem.name} |</span>
+          ))}
+      </div>
+    </li>
+  )})
   } else if (isError) {
     content = <div id="error-page">Что-то пошло не так.. {'message' in error ? error.message : ''}</div>//проверка на undefined
   }
@@ -114,10 +130,11 @@ const ItemsList = () => {
       <ol>
         <div className="sort-div">
           <select className="platfotm" onChange={(e) => setPlatform(e.target.value)}>
+            <option value=''>платформа</option>
             <option>PC</option>
-            <option>PS-5</option>
-            <option>PS-4</option>
-            <option>PS-3</option>
+            <option>PlayStation 5</option>
+            <option>PlayStation 4</option>
+            <option>PlayStation 3</option>
             <option>Xbox Series S/X</option>
             <option>Xbox 360</option>
             <option>macOS</option>
@@ -125,22 +142,25 @@ const ItemsList = () => {
             <option>Linux</option>
           </select>
           <select className="single-mmo" onChange={(e) => setGameType(e.target.value)}>
-            <option>singleplayer</option>
-            <option>multiplayer</option>
-            <option>co-op</option>
+            <option value=''>режимы</option>
+            <option>Singleplayer</option>
+            <option>Multiplayer</option>
+            <option>Co-op</option>
           </select>
           <select className="rating" onChange={(e) => setRating(e.target.value)}>
+            <option value=''>рейтинг</option>
             <option> &lt;4 rate</option>
             <option> &gt;4 rate</option>
           </select>
           <select className="ru" onChange={(e) => setLanguage(e.target.value)}>
+            <option value=''>язык</option>
             <option>ru</option>
             <option>eng</option>
           </select>
         </div>
         {content}
-        {/* <button className={isLoading ? 'active-none' : 'active'} onClick={handleClick}>Load more games</button> */}
-        {isFetching && (<ul className="newposts-loading">loading...</ul>)}
+        {/* <button className={isLoading ? 'active-none' : 'active'} onClick={handleClick}>Загрузить ещё</button> */}
+        {isFetching && (<ul className="newposts-loading">Загрузка...</ul>)}
         <FloatButton.BackTop />
       </ol>
     </section>
